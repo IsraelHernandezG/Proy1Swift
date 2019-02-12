f3704691a7d66125dcfa25849a01501169cf64b0
IsraelHernandezG <israel.hernandez.414a@gmail.com>
2019-02-11 18:18:44 -0600

    Cambio vistas listo

--- iRPG/GameScene.swift
+++ iRPG/GameScene.swift
@@ -10,9 +10,24 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
-        
+    
+        //Scene
+        var CavernBackground = SKTileMapNode()
+    
+        var movX = 0
+        var movY = 0
+    
         //Player
-        var appleNode: SKSpriteNode?
+        var appleNode = SKSpriteNode()
+        var playerNode = SKSpriteNode()
+        let texturePlayerN = SKTexture(imageNamed: "player0")
+        let texturePlayerNE = SKTexture(imageNamed: "player1")
+        let texturePlayerE = SKTexture(imageNamed: "player2")
+        let texturePlayerSE = SKTexture(imageNamed: "player3")
+        let texturePlayerS = SKTexture(imageNamed: "player4")
+        let texturePlayerSW = SKTexture(imageNamed: "player5")
+        let texturePlayerW = SKTexture(imageNamed: "player6")
+        let texturePlayerNW = SKTexture(imageNamed: "player7")
         //Carga de las imagenes del joystick
         var joystickStickImageEnabled = true {
             didSet {
@@ -33,42 +48,77 @@ class GameScene: SKScene {
         
         
         override func didMove(to view: SKView) {
-            /* Setup your scene here */
-            backgroundColor = UIColor.white
+            
             physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
-            rotateAnalogStick.position = CGPoint(x:-self.frame.maxX+rotateAnalogStick.radius+70, y:-self.frame.maxY+rotateAnalogStick.radius+70)
+            rotateAnalogStick.position = CGPoint(x:-self.frame.maxX+rotateAnalogStick.radius+65, y:-self.frame.maxY+rotateAnalogStick.radius+65)
             addChild(rotateAnalogStick)
+            
             
             //al mover el Joystick cambia la orientacion del jugador
             rotateAnalogStick.trackingHandler = { [unowned self] jData in
-                self.appleNode?.zRotation = jData.angular
+                self.CavernBackground.zRotation=jData.angular
+                
+                if (jData.angular >= -0.375 && jData.angular < 0.375){
+                    //vista N
+                    self.playerNode.run(SKAction.setTexture(self.texturePlayerN))
+                    self.movX = self.movX + 1
+                    
+                }else if (jData.angular >= 0.375 && jData.angular < 1.125){
+                    //vista NW
+                    self.playerNode.run(SKAction.setTexture(self.texturePlayerNW))
+                    self.movX = self.movX + 1
+                    self.movY = self.movY - 1
+                    
+                }else if (jData.angular >= 1.125 && jData.angular < 1.875){
+                   //vista W
+                    self.playerNode.run(SKAction.setTexture(self.texturePlayerW))
+                    self.movY = self.movY - 1
+                    
+                }else if (jData.angular >= 1.875 && jData.angular < 2.675){
+                    //vista SW
+                    self.playerNode.run(SKAction.setTexture(self.texturePlayerSW))
+                    self.movX = self.movX - 1
+                    self.movY = self.movY - 1
+                    
+                }else if (jData.angular >= 2.625 || jData.angular < -2.625){
+                    //vista S
+                    self.playerNode.run(SKAction.setTexture(self.texturePlayerS))
+                    
+                }else if (jData.angular >= -2.625 && jData.angular < -1.875){
+                    //vista SE
+                    self.playerNode.run(SKAction.setTexture(self.texturePlayerSE))
+                    
+                }else if (jData.angular >= -1.875 && jData.angular < -1.125){
+                    //vista E
+                    self.playerNode.run(SKAction.setTexture(self.texturePlayerE))
+                    
+                }else if (jData.angular >= -1.125 && jData.angular < -0.375){
+                    //vista NE
+                    self.playerNode.run(SKAction.setTexture(self.texturePlayerNE))
+                }
             }
-            //Al dejar de moverlo el personaje debe quedar en la misma posicion
             
+            
+            //self.CavernBackground.position = CGPoint(x: movX, y: movY)
             
             joystickStickImageEnabled = true
             joystickSubstrateImageEnabled = true
             
-            //inicia al personaje
-            addApple(CGPoint(x: frame.midX, y: frame.midY))
+            addPlayer(CGPoint(x: frame.midX, y: frame.midY))
             
             view.isMultipleTouchEnabled = true
         }
+    
+    func addPlayer(_ position: CGPoint) {
         
-        func addApple(_ position: CGPoint) {
-            
-            guard let appleImage = UIImage(named: "apple") else {
-                return
-            }
-            
-            let texture = SKTexture(image: appleImage)
-            let apple = SKSpriteNode(texture: texture)
-            apple.physicsBody = SKPhysicsBody(texture: texture, size: apple.size)
-            apple.physicsBody!.affectedByGravity = false
-            apple.position = position
-            addChild(apple)
-            appleNode = apple
-        }
+        let player = SKSpriteNode(texture: texturePlayerS)
+        player.physicsBody = SKPhysicsBody(texture: texturePlayerS, size: player.size)
+        player.physicsBody!.affectedByGravity = false
+        player.position = position
+        addChild(player)
+        playerNode = player
+        
+    }
         
     
     
@@ -119,5 +169,16 @@ class GameScene: SKScene {
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
+        let position=playerNode.position
+        let column = CavernBackground.tileColumnIndex(fromPosition: position)
+        let row = CavernBackground.tileRowIndex(fromPosition: position)
+        
+        let tile = CavernBackground.tileDefinition(atColumn: column, row: row)
+        
+        CavernBackground.position = CGPoint(x: movX, y: movY)
+        //if tile == pared -> Deten el movimento
+        
+        
+        
     }
 }
